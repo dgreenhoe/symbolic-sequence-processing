44ebce5053792671ec19f0fe3f7821163ee7329f
Daniel J. Greenhoe <dgreenhoe@pmsi.cc>
2020-12-01 04:45:38 -0500

    FASTA data

--- main.cpp
+++ main.cpp
@@ -103,22 +103,66 @@ TEST( LabOCS, die )
 }
 
 //---------------------------------------------------------------------------
-//! \brief Make plot TeX files
-//! \details These files can be compiled using xelatex to make pdf files 
-//!          for inclusion into main pdf file for paper
+//! \brief Analysis of pseudo-randomly generated DNA sequence
 //---------------------------------------------------------------------------
-TEST( LabOCS, dna )
+TEST( LabDNA, prng )
 {
-  lab_dna_ocs  (    0x5EED, 16000, "plots\\dna");                                                                              // Example 3.10 (artificial DNA  sequence), 3 plots
-  lab_dna_ocs  (                   "..\\..\\common\\symseq\\fasta\\NC004718_sars.dat","plots\\dna_sars");                      // Example 3.11 (SARS virus)
-  lab_dna_ocs  (                   "..\\..\\common\\symseq\\fasta\\AF086833_ebola.dat","plots\\dna_ebola");                    // Example 3.12 (Ebola virus)
-  lab_dna_ocs  (                   "..\\..\\common\\symseq\\fasta\\gi880815890_MelissococcusPlutonius.dat","plots\\dna_mpbacterium"); // Example 3.13 (Bacterium)
-  lab_dnan_ocs (                   "..\\..\\common\\symseq\\fasta\\gi187567196_papaya1446.dat","plots\\dna_papaya1446");        // Example 3.14 (papaya)
-  lab_dna_nonstatCT(0x5eed, 12000, 1200, 24, -1, 5, "plots\\dnadft");                                                           // Example 4.10 (DFT analyis of DNA sequence)
-  lab_dna_dft(                     "..\\..\\common\\symseq\\fasta\\AF086833_ebola.dat","plots\\dna_AF086833_ebola_dft");        // Example 4.11 (DFT of Ebola DNA sequence)
-  lab_dna_edge(0x5eed, 12000, 4000, 200, 17, "plots\\dnahaar");                                                                 // Example 4.13 (Wavelet analysis of DNA sequence)
-  lab_dna_averaging(1600,          "..\\..\\common\\symseq\\fasta\\NC001416_phagelambda.dat","plots\\dna_NC001416_phagelambda");// Example 4.14 (sliding histogram of Phage Lambda)
-  lab_dna_edge(     1600,          "..\\..\\common\\symseq\\fasta\\NC001416_phagelambda.dat","plots\\dna_NC001416_phagelambda");// Example 4.14 (sliding histogram of Phage Lambda)
-  lab_dna_edge(     4000,          "..\\..\\common\\symseq\\fasta\\NC001416_phagelambda.dat","plots\\dna_NC001416_phagelambda");// Example 4.14 (sliding histogram of Phage Lambda)
+  const unsigned seed = 0x5EED;
+  lab_dna_ocs  (     seed, 16000,                   "plots\\dna"     );
+  lab_dna_nonstatCT( seed, 12000, 1200, 24 , -1, 5, "plots\\dnadft"  );
+  lab_dna_edge(      seed, 12000, 4000, 200, 17,    "plots\\dnahaar" );
+}
+
+//---------------------------------------------------------------------------
+//! \brief Melissococcus Plutonius bacterium DNA analysis
+//---------------------------------------------------------------------------
+TEST( LabDNA, mpbacterium )
+{
+  lab_dna_ocs  ( "..\\fasta\\gi880815890_MelissococcusPlutonius.fasta", "plots\\dna_mpbacterium" );
+}
+
+//---------------------------------------------------------------------------
+//! \brief Phage-Lambda DNA analysis
+//! \details Sliding histogram of Phage Lambda
+//---------------------------------------------------------------------------
+TEST( LabDNA, phagelambda )
+{
+  lab_dna_averaging( 1600, "..\\fasta\\NC001416_phagelambda.fasta", "plots\\dna_NC001416_phagelambda" );
+  lab_dna_edge(      1600, "..\\fasta\\NC001416_phagelambda.fasta", "plots\\dna_NC001416_phagelambda" );
+  lab_dna_edge(      4000, "..\\fasta\\NC001416_phagelambda.fasta", "plots\\dna_NC001416_phagelambda" );
+}
+
+//---------------------------------------------------------------------------
+//! \brief Ebola DNA analysis
+//---------------------------------------------------------------------------
+TEST( LabDNA, ebola )
+{
+  lab_dna_ocs( "..\\fasta\\AF086833_ebola.fasta", "plots\\dna_ebola"              );
+  lab_dna_dft( "..\\fasta\\AF086833_ebola.fasta", "plots\\dna_AF086833_ebola_dft" );
+}
+
+//---------------------------------------------------------------------------
+//! \brief SARS DNA analysis
+//---------------------------------------------------------------------------
+TEST( LabDNA, sars )
+{
+  lab_dna_ocs( "..\\fasta\\NC004718_sars.fasta", "plots\\dna_sars" );
+}
+
+//---------------------------------------------------------------------------
+//! \brief Papaya DNA-N analysis
+//---------------------------------------------------------------------------
+TEST( LabDNA, papaya )
+{
+  lab_dnan_ocs( "..\\fasta\\gi187567196_papaya1446.fasta", "plots\\dna_papaya1446" ); 
+}
+
+//-----------------------------------------------------------------------------
+//! \brief SARS-CoV-2 DNA analysis
+//-----------------------------------------------------------------------------
+TEST( LabDNA, covid )
+{
+  lab_dna_ocs( "..\\fasta\\GenBank_MT072688-1_SARS-COV-2.fasta", "plots\\dna_SARSCoV2"     );
+  lab_dna_dft( "..\\fasta\\GenBank_MT072688-1_SARS-COV-2.fasta", "plots\\dna_SARSCoV2_dft" ); 
 }
 
